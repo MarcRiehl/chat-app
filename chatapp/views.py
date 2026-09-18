c3c7ea256641912da495a146e4457fe8b9f34efd
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-09-18 09:00:43 +0200

    Implement chat view with GET and POST methods for handling chat messages

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from .models import Chat
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 def chat_view(request):
-    chats = Chat.objects.all()
+
+    if request.method == "GET":
+        chats = Chat.objects.values( #values als Dictionary
+            "id",
+            "name",
+            "message",
+            "created_at"
+        )
+        return JsonResponse(list(chats), safe=False) #umwandlung von QuerySet
+
+
+    if request.method == "POST":
+        data = json.loads(request.body)
+        chat = Chat.objects.create(
+            name=data["name"],
+            message=data["message"]
+        )
+
+        return JsonResponse({
+            "id": chat.id,
+            "name": chat.name,
+            "message": chat.message,
+            "created_at": chat.created_at
+        })
